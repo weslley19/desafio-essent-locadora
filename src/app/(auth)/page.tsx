8856552a4c3b514fa31be0eb25dee62e31d913af
Weslley Oliveira <weslley.pereira@sigcorp.com.br>
2024-05-14 02:38:13 -0300

    integrando dashboard

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,13 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { auth } from "../../../auth";
 import { redirect } from "next/navigation";
+import { getMovies } from "./movies/action";
+import { getRented } from "./rented/actions";
 
 export default async function Home() {
   const session = auth()
-  console.log(session)
   if (!session) {
     redirect('/site')
   }
+
+  const movies = await getMovies()
+  const rented = await getRented()
 
   return (
     <div className="grid lg:grid-cols-3 gap-5 sm:grid-cols-1 md:grid-cols-1">
@@ -15,19 +19,21 @@ export default async function Home() {
         <CardHeader>
           <CardTitle>Total de Filmes</CardTitle>
         </CardHeader>
-        <CardContent className="flex justify-end text-3xl">20</CardContent>
+        <CardContent className="flex justify-end text-3xl">{movies.data.length ?? 0}</CardContent>
       </Card>
       <Card className="bg-container hover:border-slate-50">
         <CardHeader>
           <CardTitle>Filmes Alugados</CardTitle>
         </CardHeader>
-        <CardContent className="flex justify-end text-3xl">15</CardContent>
+        <CardContent className="flex justify-end text-3xl">{rented.data.length ?? 0}</CardContent>
       </Card>
       <Card className="bg-container hover:border-slate-50">
         <CardHeader>
           <CardTitle>Entrada</CardTitle>
         </CardHeader>
-        <CardContent className="flex justify-end text-3xl">R$ 150,00</CardContent>
+        <CardContent className="flex justify-end text-3xl">
+          R$ {rented.data.reduce((acc, curr) => acc + Number(curr.totalAmount), 0).toFixed(2)}
+        </CardContent>
       </Card>
     </div>
   );
